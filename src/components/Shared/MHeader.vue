324eebb8a3a7a9823ce3083d3db99147960f5589
Ferraz <afa00.papa@gmail.com>
2022-05-27 17:02:48 -0300

    feat: 🛠  Add keyboard shortcuts guide

--- src/components/Shared/MHeader.vue
+++ src/components/Shared/MHeader.vue
@@ -9,6 +9,22 @@
                 About
             </router-link>
         </nav>
+        <div class="navigation-control" v-if="type !== 'xs'">
+            <ul>
+                <li>
+                    <code>TAB</code>
+                    <span>Selecionar convenção de commit</span>
+                </li>
+                <li>
+                    <code>CTRL + Space</code>
+                    <span>Copiar convenção</span>
+                </li>
+                <li>
+                    <code>CRTL + I</code>
+                    <span>Focar na pesquisa</span>
+                </li>
+            </ul>
+        </div>
         <div class="utils-links">
             <button>
                 <a
@@ -31,13 +47,16 @@
 </template>
 
 <script lang="ts">
+import useBreakpoints from "@/hooks/useBreakpoints";
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
     setup() {
         const openNav = ref(false);
+        const { type } = useBreakpoints();
         return {
             openNav,
+            type,
         };
     },
 });
@@ -50,6 +69,7 @@ header.header {
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
 }
 .nav__button {
     position: fixed;
@@ -100,10 +120,49 @@ nav {
         z-index: 10;
     }
 }
+.navigation-control {
+    width: 60%;
+    margin: 0 auto;
+    ul {
+        display: flex;
+        width: 100%;
+        flex-direction: column;
+        color: #fefefe;
+        list-style: none;
+        justify-content: flex-start;
+        li {
+            padding: 1rem;
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+            font-size: 1.75rem;
+            width: 100%;
+            // width: 400px;
+            font-weight: 900;
+            code {
+                padding: 1rem;
+                width: 18rem;
+                margin: 10px 0;
+                display: inline-block;
+                background-color: #f6f8fa;
+                color: #020202;
+                border-radius: 4px;
+                padding: 0.5rem 0.7rem;
+                border-radius: 4px;
+                margin: 0 0.5rem;
+                box-shadow: 0px 4px 5px rgba($color: #000000, $alpha: 0.55);
+                font-size: 85%;
+                word-wrap: normal;
+                white-space: pre;
+                margin-right: 15px;
+            }
+        }
+    }
+}
 .utils-links {
     width: 60%;
     height: auto;
-    margin-top: 15rem;
+    margin-top: 5rem;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -113,6 +172,8 @@ nav {
         border: none;
         border-radius: 4px;
         padding: 1.25rem;
+        display: block;
+        width: 200px;
         margin: 1rem 0;
         transition: all 0.4s ease-in-out;
         a {
