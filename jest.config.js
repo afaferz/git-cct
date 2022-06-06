9aa83c305099e1bae689975de4777d38d97ba559
afaferz <afa00.papa@gmail.com>
2022-06-06 10:21:58 -0300

    build: 🚧  Add jest configs to tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
-}
+    preset: "ts-jest",
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "^~/(.*)$": "<rootDir>/src/$1",
+    },
+    moduleFileExtensions: ["js", "ts", "vue", "json"],
+    transform: {
+        "^.+\\.vue$": "@vue/vue3-jest",
+        "\\.[jt]sx?$": "ts-jest",
+    },
+    transformIgnorePatterns: ["<rootDir>/node_modules/"],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "<rootDir>/src/components/**/*.vue",
+        "<rootDir>/src/views/**/*.vue",
+    ],
+    setupFiles: ["<rootDir>/tests/setup.ts"],
+};
